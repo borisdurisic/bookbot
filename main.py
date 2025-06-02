d03d6d209d2d9587eaec879ff50ba84b61bf25fc
borisdurisic <boris.durisic@gmail.com>
2025-06-02 23:37:07 +0100

    adjusted code to print number of words in book

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    number_of_words = count_words(text)
+    print(f"{number_of_words} words found in the document")
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+def count_words(book):
+    words = book.split()
+    counter = 0
+    for word in words:
+        counter += 1
+    return counter
 
 main()
