364b78eca97694126eea3a9215eaef8a91be6839
borisdurisic <boris.durisic@gmail.com>
2025-06-02 23:27:43 +0100

    added part which reads book to console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
