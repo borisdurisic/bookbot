2a0107c112b932096862bf3956d9697648b53d08
borisdurisic <boris.durisic@gmail.com>
2025-06-03 15:13:39 +0100

    adjusted code to print text in report format

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sorted_list
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_words(text)
     number_of_chars = count_chars(text)
-    print(f"{number_of_words} words found in the document")
-    print(number_of_chars)
+    chars_sorted_list = sorted_list(number_of_chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+    for i in chars_sorted_list:
+        if not i["char"].isalpha():
+            continue
+        print(f"{i['char']}: {i['num']}")
+    print("============= END ===============")
 
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -12,3 +12,13 @@ def count_chars(text):
             char_dic[char] += 1
     
     return char_dic
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_list(chars):
+    sorted_list = []
+    for char in chars:
+        sorted_list.append({"char": char, "num": chars[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
